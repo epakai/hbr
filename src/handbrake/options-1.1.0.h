8d1abd2a478932ce0658ce7d9fc5fb43293873fa
Joshua Honeycutt <joshua.honeycutt@gmail.com>
2021-07-17 07:47:37 -0500

    Fix wrong #include

--- src/handbrake/options-1.1.0.h
+++ src/handbrake/options-1.1.0.h
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "../build_args.h"
+#include "../options.h"
 #include "../validate.h"
 
 static option_t option_v1_1_0[] =
